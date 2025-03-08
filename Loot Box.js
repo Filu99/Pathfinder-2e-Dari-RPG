fb2e46c84241443c3f6765603a056a405a703b3a
Filu99 <kamilwrona28@gmail.com>
2025-03-08 19:01:17 +0100

    Update

--- Loot Box.js
+++ Loot Box.js
@@ -1,17 +1,17 @@
-
-let tableName = "TestItem"
+let tableName
 let target = token.center
 let tableSize = 0
 let lootPick
 let lootArray = new Array()
 
+//DIALOG BOX WYBORU ROLLTABLE'A
 new Dialog({
     title:'LootBox Macro',
     content:`
     <form>
       <div class="form-group">
         <label>Roll Table Name</label>
-        <input type='text' name='inputField'></input>
+        <input type='text' name='inputField' autofocus></input>
 
       </div>
     </form>`,
@@ -34,14 +34,14 @@ new Dialog({
 
 function lootboxAnimation() {
 
-    lootArray = game.tables.getName(tableName).collections.results.entries().toArray()
-    rangeArray = new Array()
-    lootArray.forEach((element) => rangeArray.push(element[1].range[1]))
-    rangeArray.forEach((element) => tableSize += 1)
+    lootArray = game.tables.getName(tableName).collections.results.entries().toArray() //PRZEPISANIE DO TABLICY ZAWARTOŚCI ROLLTABLE'A
+    rangeArray = new Array()                                                           //UTWORZENIE TABLICY NA GÓRNE WARTOŚCI RANGE
+    lootArray.forEach((element) => rangeArray.push(element[1].range[1]))       //PRZEPISANIE DO TABLICY GÓRNYCH WARTOŚCI RANGE
+    rangeArray.forEach((element) => tableSize += 1)                            //PRZELICZENIE ILOŚCI OBIEKTÓW W TABLICY
 
-    let randomPick = Math.floor(Math.random() * 100) + 1;
-    console.log(randomPick)
+    let randomPick = Math.floor(Math.random() * 100) + 1;                    //LOSOWANIE LICZBY Z ZAKRESU 1-100 WŁĄCZNIE
 
+    //WYBÓR ITEMU Z LISTY NA PODSTAWIE WYLOSOWANEJ LICZBY W ODNIESIENIU DO OKREŚLONYCH RANGE'ÓW
     for (let i = 0; i < tableSize; i++) {
         if (randomPick <= rangeArray[i]) {
             lootPick = i
@@ -49,13 +49,14 @@ function lootboxAnimation() {
         }
     }
 
+    //POBRANIE DANYCH Z ROLLTABLE NA POTRZEBY ANIMACJI
     let lootImage = lootArray[lootPick][1].img
     let lootName = lootArray[lootPick][1].text
 
     //STYL NAPISÓW
     const style = {
         "fill": "white",
-        "fontFamily": "Arial Black",
+        "fontFamily": "Lato",
         "fontSize": 28,
         "strokeThickness": 2
     }
@@ -106,27 +107,27 @@ function lootboxAnimation() {
         .effect()
         .zIndex(10)
         .file(lootImage)
-        .size(15)
-        .scaleOut(10, 3000)
+        .size(10)
+        .scaleOut(10, 1500)
         .spriteRotation(90)
         .atLocation({x: target.x, y: target.y + 100})
         .moveTowards({x: target.x, y: target.y - 150})
-        .duration(3000)
+        .duration(1500)
         .delay(2500)
 
         .effect()
         .file(lootImage)
         .zIndex(15)
-        .size(150)
+        .size(100)
         .atLocation({x: target.x, y: target.y - 150})
-        .delay(5499)
-        .duration(5000).fadeOut(1000)
+        .delay(3999)
+        .duration(4000).fadeOut(1000)
 
         .scrollingText()
         .atLocation({x: target.x, y: target.y - 225})
         .text(lootName, style)
         .duration(4000)
-        .delay(5499)
+        .delay(3999)
 
         .play()
 }
